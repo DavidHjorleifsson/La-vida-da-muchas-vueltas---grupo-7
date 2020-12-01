bd616d045f34d9e449467cf891ab1cf2b463653b
DavidHjorleifsson <2904davidh@gmail.com>
2020-12-01 21:03:22 +0100

    valiables main + fichero
    He nombrado las variables en el main y he creado el fichero FuncionesPersona

---
+++ La_vida_da_muchas_vueltas-grupo7/src/main/java/com/mycompany/la_vida_da_muchas_vueltas/grupo7/FuncionesPersona.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.la_vida_da_muchas_vueltas.grupo7;
+
+/**
+ *
+ * @author 2904d
+ */
+public class FuncionesPersona {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    public static void generoAleatorio(){
+        
+    }
+}
