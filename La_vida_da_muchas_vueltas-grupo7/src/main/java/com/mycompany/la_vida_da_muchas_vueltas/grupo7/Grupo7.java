37fff646d4a25ddfc9f672af3ce137cffdbeeb2d
DavidHjorleifsson <2904davidh@gmail.com>
2020-12-02 20:49:12 +0100

    Creado pero no funciona
    He seguido los requisitos del ejercicio, pero NO FUNCIONA. Esta todo hecho en el fichero grupo 7 xq no se como hacer que funcione desde otro fichero. SOLO TE PIDO Q LO ARREGLES.

--- La_vida_da_muchas_vueltas-grupo7/src/main/java/com/mycompany/la_vida_da_muchas_vueltas/grupo7/Grupo7.java
+++ La_vida_da_muchas_vueltas-grupo7/src/main/java/com/mycompany/la_vida_da_muchas_vueltas/grupo7/Grupo7.java
@@ -5,23 +5,41 @@
  */
 package com.mycompany.la_vida_da_muchas_vueltas.grupo7;
 
+import java.util.Random;
+
 /**
  *
  * @author 2904d
  */
 public class Grupo7 {
-
-    /**
-     * @param args the command line arguments
-     */
-    
-    public static void main(String[] args) {
-        // TODO code application logic here
-        String nombreYo="";
-        String apellidosYO="";
-        boolean generoYo;
-        
+  
+    public static void generoAleatorio(){        
+        Random alG= new Random();
+        String generoYo=alG.nextBoolean()?"Hombre":"Mujer";
     }
+    
+    public static void nombreAleatorio(){       
+        Random alG= new Random();
+        String generoYo=alG.nextBoolean()?"Hombre":"Mujer";
+        
+        if(generoYo.equals("Hombre")){
+            String nombreYo[]={"MatÃ­as","Luis","Manolo","Paco","Jose","David","Pablo","NÃ©stor","Pepe","Antonio","Francisco"};
+            System.out.println("Nombre: "+nombreYo);
+        }else{
+            String nombreYo[]={"Marta","Lola","Ana","Rosa","Angustias","Laura","Sara"};
+            System.out.println("Nombre: "+nombreYo);
+        }   
+    }
+    public static void apellidosAleatorio(){
+        String apellidosYo[]={"RodrÃ­guez","Poveda","MartÃ­n","GÃ³mez","Monedero","Aguilar","Luque","De Grado"};
+        String apellidosYo2[]={"Fernández","Diaz","Moreno","Torres","Ortega","Suarez","Cortes","Cano"};
+        System.out.print(" "+apellidosYo+" "+apellidosYo2);
+    }
+    public static void main(String[] args) {       
+        nombreAleatorio();
+        apellidosAleatorio();
+    }
+    
     
     
 }
